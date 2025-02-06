fba4b01354f49acd118d7c64052719809479c700
quaswexexort1 <coldzerokot@gmail.com>
2025-02-06 19:32:21 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿
-public class ZNAK : ICloneable, IComparable <ZNAK>
+public class ZNAK : ICloneable, IComparable 
 {
     public string Familiya { get; set; }
     public string Name { get; set; }
@@ -16,7 +15,7 @@ public class ZNAK : ICloneable, IComparable <ZNAK>
 
     public override string ToString()
     {
-        return $"{Familiya} {Name} ({ZnakZodiaca}), Month: {Birthday}";
+        return $"Фамилия: {Familiya},Имя: {Name},Знак зодиака: {ZnakZodiaca}, Месяц рождения: {Birthday}";
     }
 
     // ICloneable
@@ -26,12 +25,88 @@ public class ZNAK : ICloneable, IComparable <ZNAK>
     }
 
     // IComparable
-    public int CompareTo(ZNAK ?other)
+    public int CompareTo(object ?obj)
     {
-        if (other == null)
+        if (obj == null)
             return 1;
 
-        return ZnakZodiaca.CompareTo(other.ZnakZodiaca);
+        ZNAK otherZnak = obj as ZNAK;
+        if (otherZnak != null)
+        {
+            return ZnakZodiaca.CompareTo(otherZnak.ZnakZodiaca);
+        }
+
+        else
+        {
+            throw new ArgumentException("не ZNAK");
+        }
+
     }
+}
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        int arraySize = 3;
+        ZNAK[] znakArray = new ZNAK[arraySize];
+
+        for (int i = 0; i < arraySize; i++)
+        {
+            Console.WriteLine($"Человек: {i + 1}:");
+
+            Console.Write("Фамилия: ");
+            string familiya = Console.ReadLine()!;
+
+            Console.Write("Имя: ");
+            string name = Console.ReadLine()!;
+
+            Console.Write("Знак зодиака: ");
+            string znakzodiaca = Console.ReadLine()!;
+
+            Console.Write("Месяц рождения (1-12): ");
+            string birthday = Console.ReadLine()!;
+
+            int month = int.Parse(Console.ReadLine()!);
+
+            znakArray[i] = new ZNAK(familiya, name, znakzodiaca, month);
+        }
+
+
+        // Сортировка по знаку зодиака
+        Array.Sort(znakArray);
+
+        // Вывод
+        Console.WriteLine("\n Сортировка по знаку зодиака: ");
+        foreach (var znak in znakArray)
+        {
+            Console.WriteLine(znak);
+        }
+
+
+
+
+        // Поиск по месяцу
+        Console.Write("\n Сортировка по месяцу: ");
+        int searchMonth = int.Parse(Console.ReadLine()!);
+
+        // Вывод
+        Console.WriteLine($"\n Люди, рожденные в месяце: {searchMonth}");
+        bool found = false;
+        foreach (var znak in znakArray)
+        {
+            if (znak.Birthday == searchMonth) // Если месяц рождения совпадает 
+            {
+                Console.WriteLine(znak);
+                found = true;
+            }
+        }
+
+        if (!found)
+        {
+            Console.WriteLine("Никто не найден в этом месяце.");
+        }
+    }
+
 }
 
