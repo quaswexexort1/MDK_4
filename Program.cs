30e0f81f90a5f610bf25ce7b4c24f02f62286236
idk <idk@209-17>
2025-02-06 18:48:31 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿
+public class ZNAK : ICloneable, IComparable <ZNAK>
+{
+    public string Familiya { get; set; }
+    public string Name { get; set; }
+    public string ZnakZodiaca { get; set; }
+    public int Birthday { get; set; }
+
+    public ZNAK(string familiya, string name, string znakzodiaca, int birthday)
+    {
+        Familiya = familiya;
+        Name = name;
+        ZnakZodiaca = znakzodiaca;
+        Birthday = birthday;
+    }
+
+    public override string ToString()
+    {
+        return $"{Familiya} {Name} ({ZnakZodiaca}), Month: {Birthday}";
+    }
+
+    // ICloneable
+    public object Clone()
+    {
+        return new ZNAK(Familiya, Name, ZnakZodiaca, Birthday);
+    }
+
+    // IComparable
+    public int CompareTo(ZNAK ?other)
+    {
+        if (other == null)
+            return 1;
+
+        return ZnakZodiaca.CompareTo(other.ZnakZodiaca);
+    }
+}
+
